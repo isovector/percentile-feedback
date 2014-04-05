c1dc9f557181f22795286779ba0432fff846696e
Noah Slater <nslater@tumbolia.org>
2014-04-05 21:27:19 +0200

    Fix Highcharts oddities

--- work_timer.js
+++ work_timer.js
@@ -236,11 +236,12 @@ function generatePercentileWorkChart(past_histogram, today_histogram, today_perc
         text: 'Hour, starting at ' + midnight
       },
       tickInterval: 1,
+      tickmarkPlacement: "on",
       startOnTick: false,
       endOnTick: false,
       showLastLabel: true,
       min: 0.5 + compensation,
-      max: 24 + compensation,
+      max: 22.5 + compensation,
       categories: ["00", "01", "02", "03", "04", "05",
                    "06", "07", "08", "09", "10", "11",
                    "12", "13", "14", "15", "16", "17",
