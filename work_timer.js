a3810253361de235ff32e0a2e1770e368a405886
Noah Slater <nslater@tumbolia.org>
2014-04-05 21:16:00 +0200

    Add hour x-axis labels

--- work_timer.js
+++ work_timer.js
@@ -235,12 +235,18 @@ function generatePercentileWorkChart(past_histogram, today_histogram, today_perc
         enabled: true,
         text: 'Hour, starting at ' + midnight
       },
-      tickInterval: 2,
+      tickInterval: 1,
       startOnTick: false,
       endOnTick: false,
       showLastLabel: true,
-      min: 0 + compensation,
-      max: 24 + compensation
+      min: 0.5 + compensation,
+      max: 24 + compensation,
+      categories: ["00", "01", "02", "03", "04", "05",
+                   "06", "07", "08", "09", "10", "11",
+                   "12", "13", "14", "15", "16", "17",
+                   "18", "19", "20", "21", "22", "23",
+                   "00", "02", "03", "04", "05", "06",
+                   "07", "08", "09", "10", "11", "12"]
     },
     yAxis: {
       title: {
